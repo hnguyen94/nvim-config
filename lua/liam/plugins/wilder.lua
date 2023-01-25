b3e4b6ccf795773ee07ceff563ce5c05b97aa51d
Hoang D. Nguyen <hdn.account@icloud.com>
2023-01-25 11:33:30 +1100

    Add highlighter to wilder

--- lua/liam/plugins/wilder.lua
+++ lua/liam/plugins/wilder.lua
@@ -20,6 +20,32 @@ wilder.set_option("pipeline", {
 	})),
 })
 
+local gradient = {
+	"#f4468f",
+	"#fd4a85",
+	"#ff507a",
+	"#ff566f",
+	"#ff5e63",
+	"#ff6658",
+	"#ff704e",
+	"#ff7a45",
+	"#ff843d",
+	"#ff9036",
+	"#f89b31",
+	"#efa72f",
+	"#e6b32e",
+	"#dcbe30",
+	"#d2c934",
+	"#c8d43a",
+	"#bfde43",
+	"#b6e84e",
+	"#aff05b",
+}
+
+for i, fg in ipairs(gradient) do
+	gradient[i] = wilder.make_hl("WilderGradient" .. i, "Pmenu", { { a = 1 }, { a = 1 }, { foreground = fg } })
+end
+
 wilder.set_option(
 	"renderer",
 	wilder.renderer_mux({
@@ -36,7 +62,12 @@ wilder.set_option(
 			-- 	wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
 			-- 	-- at https://github.com/romgrk/fzy-lua-native
 			-- },
-			highlighter = wilder.basic_highlighter(),
+			highlights = {
+				gradient = gradient,
+			},
+			highlighter = wilder.highlighter_with_gradient({
+				wilder.basic_highlighter(),
+			}),
 		})),
 		["/"] = wilder.wildmenu_renderer({
 			highlighter = wilder.basic_highlighter(),
