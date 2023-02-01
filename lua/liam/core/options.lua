f9ffb66be4a342131e43ba4363ca4a64648e8689
Hoang D. Nguyen <hdn.account@icloud.com>
2023-02-01 11:30:32 +1100

    Autosave when buffer is not in focus

--- lua/liam/core/options.lua
+++ lua/liam/core/options.lua
@@ -38,3 +38,12 @@ opt.splitbelow = true
 vim.cmd("language en_AU.UTF-8")
 
 opt.iskeyword:append("-")
+
+-- Autosave when leave buffer
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+	callback = function()
+		if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+			vim.api.nvim_command("silent update")
+		end
+	end,
+})
